3de28171c724ab2f78940a480dbdf2ce0f3bd07a
FikuSS14 <dima.aparin.98@mail.ru>
2025-08-18 18:59:48 +0300

    Update task.js

---
+++ event-object/tabs/task.js
@@ -0,0 +1,15 @@
+document.querySelectorAll('.tabs').forEach(tabsContainer => {
+    const tabButtons = tabsContainer.querySelectorAll('.tab');
+    const tabContents = tabsContainer.querySelectorAll('.tab__content');
+  
+    tabButtons.forEach((tab, index) => {
+      tab.addEventListener('click', () => {
+        tabButtons.forEach(t => t.classList.remove('tab_active'));
+        tabContents.forEach(c => c.classList.remove('tab__content_active'));
+  
+        tab.classList.add('tab_active');
+
+        tabContents[index].classList.add('tab__content_active');
+      });
+    });
+  });
